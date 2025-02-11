57fdfab90a0942dbe7a9c60473d88d23a59ca2df
Pramesh Pradhan <pra.mesh.dhan@hotmail.com>
2025-02-11 21:06:03 +0545

    initial day2

---
+++ Day2/Day2.md
@@ -0,0 +1,6 @@
+# Data Types and methods
+# Math objects
+- _Math.PI_ \t  // gives value of PI
+- _Math.round_ \t // rounds to closest number if above .5 up if less down rounding
+- _Math.floor_ \t // rounding down
+- _Math.ceil_ \t // rounding up
---
+++ Day2/js/maths.js
@@ -0,0 +1,15 @@
+const PI = Math.PI;
+console.log(PI);
+
+//Rounding UP
+console.log(Math.round(PI));
+
+console.log(Math.round(9.81));
+
+console.log(+PI.toFixed(2));
+
+const P2 = 7.8191;
+console.log(+P2.toFixed(1));
+
+console.log(Math.ceil(P2));
+console.log(Math.floor(P2));
